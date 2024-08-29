ab94dafd4880088faaf8309866691116a8e9d2cb
shyye <emma.art.dev@gmail.com>
2024-08-29 07:18:41 +0200

    Add code for Core part

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,32 @@
 package com.booleanuk.api.counter;
 
-public class CounterController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("counter")
+public class CounterController {
+    private int counter;
+
+    public CounterController() {
+        this.counter = 0;
+    }
+
+    @GetMapping
+    public String getCounter() {
+        return String.valueOf(this.counter);
+    }
+
+    @GetMapping("increment")
+    public String increment() {
+        this.counter++;
+        return String.valueOf(this.counter);
+    }
+
+    @GetMapping("decrement")
+    public String decrement() {
+        this.counter--;
+        return String.valueOf(this.counter);
+    }
 }
