a52357ddaadb72f793903eee1d1bfaadabe5612a
shyye <emma.art.dev@gmail.com>
2024-08-29 07:49:57 +0200

    Add code for Extension part

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,16 +1,21 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("counter")
 public class CounterController {
     private int counter;
+    private HashMap<String, Integer> counterMap;
 
     public CounterController() {
         this.counter = 0;
+        this.counterMap = new HashMap<>();
     }
 
     @GetMapping
@@ -28,5 +33,35 @@ public class CounterController {
     public String decrement() {
         this.counter--;
         return String.valueOf(this.counter);
+    }
+
+    @GetMapping("custom/{name}")
+    public String getSpecificCounter(@PathVariable String name) {
+        if (this.counterMap.get(name) == null) {
+            this.counterMap.put(name, 0);
+        }
+        return name+": " + this.counterMap.get(name);
+    }
+
+    @GetMapping("custom/{name}/increment")
+    public String specificIncrement(@PathVariable String name) {
+        if (this.counterMap.get(name) == null) {
+            this.counterMap.put(name, 1);
+        } else {
+            int counterValue = this.counterMap.get(name);
+            this.counterMap.put(name, counterValue + 1);
+        }
+        return name+": " + this.counterMap.get(name);
+    }
+
+    @GetMapping("custom/{name}/decrement")
+    public String specificDecrement(@PathVariable String name) {
+        if (this.counterMap.get(name) == null) {
+            this.counterMap.put(name, -1);
+        } else {
+            int counterValue = this.counterMap.get(name);
+            this.counterMap.put(name, counterValue - 1);
+        }
+        return name+": " + this.counterMap.get(name);
     }
 }
